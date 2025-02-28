1df44dfd438a6d165cc6933d764ba10c7c0745ac
HEMANTHKUMARANNAM <hemanthkumar312004@gmail.com>
2025-02-28 23:22:36 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Admin from './Admin';
+// import Admin from './Admin';
 
 import Home from "./Hack"
 
